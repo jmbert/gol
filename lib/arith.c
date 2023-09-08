ae23da32d7feeba3374e0aaa22ba33bcec75a14e
jmbert <joe2157b@gmail.com>
2023-09-08 20:55:02 +0100

    Fixed other arithmetic operators

--- lib/arith.c
+++ lib/arith.c
@@ -13,13 +13,13 @@ char *_add_lisp(int argc, char **argv) {
 }
 
 char *_sub_lisp(int argc, char **argv) {
-    int result = 0;
-    if (argc > 1) {
-        result = atoi(argv[argc-1]);
-    }
-
-    for (int i = argc-2; i > 1; i--) {
-        result -= atoi(argv[i]);
+    int result = atoi(argv[argc-1]);
+    if (argc == 1) {
+        result = -result;
+    } else {
+        for (int i = argc-2; i >= 0; i--) {
+            result -= atoi(argv[i]);
+        }
     }
     int size = snprintf(NULL, 0, "%d", result);
     char *out = (char*)malloc(sizeof(char)*size);
@@ -28,7 +28,7 @@ char *_sub_lisp(int argc, char **argv) {
 }
 
 char *_mul_lisp(int argc, char **argv) {
-    int result = 0;
+    int result = 1;
     for (int i = argc-1; i >= 0; i--) {
         result *= atoi(argv[i]);
     }
@@ -39,11 +39,8 @@ char *_mul_lisp(int argc, char **argv) {
 }
 
 char *_div_lisp(int argc, char **argv) {
-    int result = 0;
-    result = atoi(argv[argc-1]);
-    
-
-    for (int i = argc-1; i >= 0; i--) {
+    int result = atoi(argv[argc-1]);
+    for (int i = argc-2; i >= 0; i--) {
         result /= atoi(argv[i]);
     }
     int size = snprintf(NULL, 0, "%d", result);
